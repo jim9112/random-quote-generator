91d876f123e0a5d7c042b3dfb8ed6a610a4fbe12
jim9112 <jim9112@gmail.com>
2018-05-18 11:07:33 -0400

    Update color change background

--- js/quoteList.js
+++ js/quoteList.js
@@ -1,33 +1,36 @@
-var quotes = [
-    {
-        quote: 'placeholder1',
-        source: 'placeholder',
-        citation: 'placeholder',
-        date: '1900',
+var quotes = [{
+        quote: 'It does not matter how slowly you go as long as you do not stop.',
+        source: 'Confucius',
+        citation: '',
+        date: '',
+        retrieved: 'https://www.brainyquote.com/quotes/confucius_140908',
     },
     {
-        quote: 'placeholder2',
-        source: 'placeholder',
-        citation: 'placeholder',
+        quote: 'It is during our darkest moments that we must focus to see the light.',
+        source: 'Aristotle',
+        citation: '',
         date: '',
+        retrieved: 'https://www.brainyquote.com/quotes/aristotle_119068?src=t_inspirational',
     },
     {
         quote: 'placeholder3',
         source: 'placeholder',
         citation: 'placeholder',
         date: '1900',
+        retrieved: ''
     },
     {
         quote: 'placeholder4',
         source: 'placeholder',
         citation: 'placeholder',
         date: '1900',
+        retrieved: ''
     },
     {
         quote: 'placeholder5',
         source: 'placeholder',
         citation: 'placeholder',
         date: '1900',
+        retrieved: ''
     }
 ];
-
--- js/script.js
+++ js/script.js
@@ -2,44 +2,55 @@
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
-function print(message) {
-    var div = document.getElementById('output');
-    div.innerHTML = message;
-  }
+// call printQuote function so page loads with a quote
+printQuote();
 
-// generate a random number between 1 and 4
+// generate a random number between 1 and 4 ( didnt add +1 at end so I could get 0 as an option)
 function randomNum() {
-    return Math.floor(Math.random() * 5 );
+    return Math.floor(Math.random() * 5);
 }
-
-
-// test random number 
-console.log(randomNum());
-
 
 // pull random quote
 function getRandomQuote(quote) {
     var quote = quotes[randomNum()];
     return quote;
 }
-
-// test random quote
-console.log(getRandomQuote());
-
-
+function randomColorNum() {
+    return Math.floor(Math.random() * 256 );
+}
+function randomColor() {
+ var color1 = randomColorNum();
+ var color2 = randomColorNum();
+ var color3 = randomColorNum();   
+ document.body.style.backgroundColor = "rgb(" + color1+","+ color2+","+ color3+")";
+ document.getElementById("loadQuote").style.backgroundColor = "rgb(" + color1+","+ color2+","+ color3+")";
+ console.log(color1);
+}
 
 // function to print quote
 function printQuote() {
     var quoteToPrint = getRandomQuote();
     var display = '<p class="quote">' + quoteToPrint.quote + '</p>';
     display += '<p class="source">' + quoteToPrint.source;
-    if (quoteToPrint.citation.length > 0){
-        display += '<span class="citation">'+ quoteToPrint.citation + '</span>'
+
+    // displays citation if available
+    if (quoteToPrint.citation.length > 0) {
+        display += '<span class="citation">' + quoteToPrint.citation + '</span>'
     }
-    if (quoteToPrint.date.length > 0){
-        display += '<span class="year">'+ quoteToPrint.date + '</span>'
+
+    // displays year of quote if available
+    if (quoteToPrint.date.length > 0) {
+        display += '<span class="year">' + quoteToPrint.date + '</span>'
     }
-     display += '</p>';
+    display += '</p>';
+
+    // displays a link to where the quote was found if available
+    if (quoteToPrint.retrieved.length > 0) {
+    display += '<p class="retrieved-from">Retrieved From: <a href="' + quoteToPrint.retrieved + '">'+ quoteToPrint.retrieved +'t</a></p>';
+    }
     var div = document.getElementById('quote-box');
     div.innerHTML = display;
+
+    randomColor();
 }
+// document.body.style.backgroundColor = "red";
