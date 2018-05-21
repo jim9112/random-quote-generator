3948f1d86b0997743df832740940948188911d59
jim9112 <jim9112@gmail.com>
2018-05-20 20:05:57 -0400

    update syntax

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
 // changes quote and background color every 30 seconds automatically
-var intervalID = window.setInterval(printQuote, 30000);
+let intervalID = window.setInterval(printQuote, 30000);
 
 // call printQuote function so page loads with a quote
 printQuote();
@@ -26,37 +26,37 @@ function randomColorNum() {
 
 // random color generator for background
 function randomColor() {
- var color1 = randomColorNum();
- var color2 = randomColorNum();
- var color3 = randomColorNum();   
+ let color1 = randomColorNum();
+ let color2 = randomColorNum();
+ let color3 = randomColorNum();   
  document.body.style.backgroundColor = "rgb(" + color1+","+ color2+","+ color3+")";
  document.getElementById("loadQuote").style.backgroundColor = "rgb(" + color1+","+ color2+","+ color3+")";
 }
 
 // function to print quote
 function printQuote() {
-    var quoteToPrint = getRandomQuote();
-    var display = '<p class="quote">' + quoteToPrint.quote + '</p>';
-    display += '<p class="source">' + quoteToPrint.source;
+    let quoteToPrint = getRandomQuote();
+    let display = `<p class="quote">${quoteToPrint.quote}</p>`;
+    display += `<p class="source"> ${quoteToPrint.source}`;
 
     // displays citation if available
     if (quoteToPrint.citation.length > 0) {
-        display += '<span class="citation">' + quoteToPrint.citation + '</span>'
+        display += `<span class="citation">${quoteToPrint.citation }</span>`;
     }
 
     // displays year of quote if available
     if (quoteToPrint.year.length > 0) {
-        display += '<span class="year">' + quoteToPrint.year + '</span>'
+        display += `<span class="year">${quoteToPrint.year}</span>`
     }
 
     // close paragraph statement
-    display += '</p>';
+    display += `</p>`;
 
     // displays a link to where the quote was found if available
     if (quoteToPrint.retrieved.length > 0) {
-    display += '<p class="retrieved-from">Retrieved From: <a href="' + quoteToPrint.retrieved + '">'+ quoteToPrint.retrieved +'t</a></p>';
+    display += `<p class="retrieved-from">Retrieved From: <a href="${quoteToPrint.retrieved}">${quoteToPrint.retrieved}</a></p>`;
     }
-    var div = document.getElementById('quote-box');
+    let div = document.getElementById('quote-box');
     div.innerHTML = display;
 
     // calls function to change background color
